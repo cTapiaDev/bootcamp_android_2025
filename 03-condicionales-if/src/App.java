52d46e4a5fe5219613885605e8f5b2635a1355de
Carlos Tapia <ctapiadev@gmail.com>
2025-11-07 21:43:23 -0300

    switch

--- 03-condicionales-if/src/App.java
+++ 03-condicionales-if/src/App.java
@@ -65,12 +65,10 @@ public class App {
          * Si no cumple ninguna, es una "F".
          */
 
-        int notaAlumno = 22;
+        int notaAlumno = 82;
         char calificacion;
 
         System.out.println("Revisando la nota: " + notaAlumno);
-
-        System.out.println(notaAlumno >= 80);
 
         if (notaAlumno >= 90) {
             calificacion = 'A';
@@ -86,5 +84,176 @@ public class App {
 
         System.out.println("La calificación en letra es: " + calificacion);
 
+
+        System.out.println("\n--------------------------");
+        System.out.println("--------------------------\n");
+        System.out.println("\n--------------------------");
+        System.out.println("--------------------------\n");
+
+        // Ejercicio 1 - Alerta de Batería Baja
+        int bateria = 38;
+        final int LIMITE_BATERIA = 20;
+
+        System.out.println("Nivel de batería actual: " + bateria + "%");
+
+        if (bateria <= LIMITE_BATERIA) {
+            System.out.println("¡ALERTA! Batería baja.");
+            System.out.println("Por favor, conecta tu cargador.");
+        }
+
+        System.out.println("...el sistema sigue funcionando...");
+
+        
+        System.out.println("\n--------------------------");
+        System.out.println("--------------------------\n");
+
+        // Ejercicio 2 - Verificador de descuento para Estudiantes
+        double precioBase = 100;
+        boolean esEstudiante = false;
+
+        final double DESCUENTO = 0.10;
+        double precioFinal;
+
+        System.out.println("Cliente comprando por $" + precioBase);
+
+        if (esEstudiante == true) {
+            System.out.println("Aplicando descuento de estudiante...");
+            double montoDescuento = precioBase * DESCUENTO;
+            precioFinal = precioBase - montoDescuento;
+        } else {
+            System.out.println("No se aplica descuento.");
+            precioFinal = precioBase;
+        }
+
+        System.out.println("El precio final a pagar es: $" + precioFinal);
+
+
+
+        System.out.println("\n--------------------------");
+        System.out.println("--------------------------\n");
+
+
+        // Ejercicio 3 - Termostato Inteligente
+        /*
+         * Opciones:
+         * - Hace frío
+         * - Está agradable
+         * - Hace calor
+         */
+
+        int temperaturaActual = 35;
+
+        final int TEMP_MIN_AGRADABLE = 18;
+        final int TEMP_MAX_AGRADABLE = 25;
+
+        String accionTermostato;
+
+        if (temperaturaActual > TEMP_MAX_AGRADABLE) {
+            // Camino: Calor
+            accionTermostato = "Encender el aire acondicionado";
+        } else if (temperaturaActual < TEMP_MIN_AGRADABLE) {
+            // Camino: Frío
+            accionTermostato = "Encender la calefacción";
+        } else {
+            // Camino: Agradable
+            accionTermostato = "Modo espera. Temperatura agradable";
+        }
+
+        System.out.println("Temperatura: " + temperaturaActual + "°C");
+        System.out.println("Acción: " + accionTermostato);
+
+
+        System.out.println("\n--------------------------");
+        System.out.println("--------------------------\n");
+
+        // Ejercicio 4 - Calculadora de Propina
+        /*
+         * Estándar es 15%
+         * Si damos menos del 5 nos va a decir que es baja
+         * Si damos más del 5 nos va a decir que es generosa 
+         * 
+         * 1- Necesitamos la cuenta - double
+         * 2- Almacenar el % de propina
+         * 3- Calcular cuanto es la propina
+         */
+
+        double cuenta = 20;
+        final double PORCENTAJE_PROPINA = 0.15;
+
+        double propina = cuenta * PORCENTAJE_PROPINA;
+        double totalConPropina = cuenta + propina;
+
+        System.out.println("--- Boleta ---");
+        System.out.println("Monto de la cuenta: $" + cuenta);
+        System.out.println("Propina (15%): $" + propina);
+        System.out.println("Total a Pagar: $" + totalConPropina);
+
+        if (propina > 5) {
+            System.out.println("Mensaje: Es una propina generosa.");
+        } else {
+            System.out.println("Mensaje: Es una propina baja.");
+        }
+
+
+        System.out.println("\n--------------------------");
+        System.out.println("--------------------------\n");
+
+
+        // Ejercicio 5 - Comisión por Ventas
+        /*
+         * Un vendedor gana comisión según cuánto vende.
+         * Si vende más de $10.000 -> gana un 10% de comisión
+         * Si vende entre $5.000 y $10.000 -> gana un 5% de comisión
+         * Si vende menos de $5.000 -> gana un 2% de comisión
+         */
+        
+        String vendedor = "Cristian";
+        double totalVentas = 10000;
+        double porcentajeComision;
+
+        if (totalVentas > 10000){
+            porcentajeComision = 0.1;
+        } else if (totalVentas >= 5000){
+            porcentajeComision = 0.05;
+        } else {
+            porcentajeComision = 0.02;
+        }
+
+        double totalComision = totalVentas * porcentajeComision;
+
+        System.out.println("El vendedor " + vendedor + " gana una comisión de " + totalComision);
+
+        System.out.println("\n--------------------------");
+        System.out.println("--------------------------\n");
+
+
+        double boleta = 10000;
+        double comisionA = 0.1;
+        double comisionB = 0.05;
+        double comisionC = 0.02;
+
+        if (boleta > 10000) {
+
+            double comision = comisionA * boleta;
+            System.out.println("Gana 10% de comision por una Boleta de: $" + boleta + " y Obtuvo una comision de: $" + comision);
+            
+        } else if(boleta <= 10000 & boleta >= 5000){
+
+            double comision = comisionB * boleta;
+            System.out.println("Gana 5% de comision por una Boleta de: $" + boleta + " y Obtuvo una comision de: $" + comision);
+        
+        } else {
+
+            double comision = comisionC * boleta;
+            System.out.println("Gana 2% de comision por una Boleta de: $" + boleta + " y Obtuvo una comision de: $" + comision);
+        }
+        
+
+
+        
+
+
+
+        
     }
 }
---
+++ 04-condicionales-switch/src/App.java
@@ -0,0 +1,141 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Operadores Lógicos
+        // && (Y), || (O) ! (NOT)
+
+        // Operador AND (&&)
+        /*
+         * Exige que AMBAS condiciones sean true.
+         * Si una sola condición es false, el resultado total es false.
+         * 
+         * true && true = true
+         * false && true = false
+         * true && false = false
+         * false && false = false
+         */
+
+        boolean carnet = true;
+        boolean entrada = true;
+
+        System.out.println("¿Tiene carnet?: " + carnet);
+        System.out.println("¿Tiene entrada?: " + entrada);
+
+        if (carnet == true && entrada == true) {
+            System.out.println("¡Bienvenido al concierto!");
+        } else {
+            System.out.println("Acceso denegado.");
+        }
+
+        System.out.println("\n------------------------");
+        System.out.println("------------------------\n");
+
+        // Operador OR (||)
+        /*
+         * Solo necesito que UNA de las condiciones sea true.
+         * Solo va a ser false si ambas son false.
+         * 
+         * true || true = true
+         * false || true = true
+         * true || false = true
+         * false || false = false
+         */
+
+        boolean esEstudiante = false;
+        boolean esJubilado = true;
+
+        System.out.println("¿Es estudiante?: " + esEstudiante);
+        System.out.println("¿Es jubilado?: " + esJubilado);
+
+        if (esEstudiante == true || esJubilado == true) {
+            System.out.println("¡Felicidades! Tienes un descuento.");
+        } else {
+            System.out.println("No tiene descuento que aplicar.");
+        }
+
+
+        System.out.println("\n------------------------");
+        System.out.println("------------------------\n");
+
+        // Operador NOT (!)
+        /*
+         * Simplemente va a invertir un booleano.
+         * 
+         * !true = false
+         * !false = true
+         * 
+         * if (!estaLloviendo)
+         */
+
+        boolean tieneMembresia = true;
+        System.out.println("¿Tiene membresia?: " + tieneMembresia);
+
+        if (!tieneMembresia) {
+            System.out.println("Por favor, pague su membresia.");
+        } else {
+            System.out.println("¡Bienvenido!");
+        }
+
+
+        System.out.println("\n------------------------");
+        System.out.println("------------------------\n");
+
+
+        // Switch
+        /*
+         * Semeja a la creación de un menú.
+         */
+
+        int diaSemana = 3;
+        String nombreDia;
+
+        switch (diaSemana) {
+            case 1: 
+                nombreDia = "Lunes";
+                break;
+            case 2:
+                nombreDia = "Martes";
+                break;
+            case 3:
+                nombreDia = "Miércoles";
+                break;
+            case 4:
+                nombreDia = "Jueves";
+                break;
+            case 5:
+                nombreDia = "Viernes";
+                break;
+            case 6:
+                nombreDia = "Sábado";
+                break;
+            case 7:
+                nombreDia = "Domingo";
+                break;
+            default:
+                nombreDia = "Día inválido";
+                break;
+        }
+
+        System.out.println("El día " + diaSemana + " es: " + nombreDia);
+
+
+        System.out.println("\n------------------------");
+        System.out.println("------------------------\n");
+
+
+        String luz = "rojo";
+
+        switch (luz) {
+            case "rojo" -> {
+                System.out.println("¡Detente!");
+            }
+            case "amarillo" -> System.out.println("Avanza con precaución");
+            case "verde" -> System.out.println("¡Avanza!");
+            default -> System.out.println("Semaforo malo.");
+        }
+
+
+
+
+    }
+}
